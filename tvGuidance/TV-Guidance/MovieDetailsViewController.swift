7d04db0796ae4993eebae6cb76b0f89f651f0b14
Farhan Tanmoy <43865401+ftanmoy@users.noreply.github.com>
2021-04-30 06:04:56 -0400

    setting up api push 2

--- tvGuidance/TV-Guidance/MovieDetailsViewController.swift
+++ tvGuidance/TV-Guidance/MovieDetailsViewController.swift
@@ -8,19 +8,55 @@
 import UIKit
 import AlamofireImage
 
-class MovieDetailsViewController: UIViewController {
+class MovieDetailsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var posterView: UIImageView!
     
     @IBOutlet weak var titleLabel: UILabel!
     
-    @IBOutlet weak var providerLabel: UILabel!
+    @IBOutlet weak var providersCollectionView: UICollectionView!
+    //    @IBOutlet weak var providerLabel: UILabel!
     
+//    @IBOutlet weak var watchProvidersCell: UICollectionViewCell!
     
     var tvShow: [String:Any]!
-    var tvShowProviders: [String: Any]!
+    var tvShowProviders = [[String: Any]]()
+    var providersCollection: [String:Any]!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        providersCollectionView.delegate = self
+        providersCollectionView.dataSource = self
+        
+        
+        let layout = providersCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
+//        calling api for watch providers
+        let url = URL(string: "https://api.themoviedb.org/3/watch/providers/tv?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+                    // TODO: Get the array of movies
+
+                self.tvShowProviders = dataDictionary["results"] as! [[String: Any]]
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
         
         titleLabel.text = tvShow["original_name"] as? String
         
@@ -30,21 +66,32 @@ class MovieDetailsViewController: UIViewController {
         
         posterView.af.setImage(withURL: posterUrl)
         
+        
+        // change the watch on section images based on the api results
+//        providersCollection = tvShowProviders["flatrate"]
+               
+//        if(tvShowProviders["provider_name"] == "fuboTV") {
+//            var image: UIImage = UIImage(named: "fubotv")!
+            
+//        }
+        
         // Do any additional setup after loading the view.
     }
     
-    func getProviders() {
-        
-        let providersApiUrl = "https://api.themoviedb.org/3/tv/"
-        let showId = tvShow["id"] as! String
-        let endUrl = "/watch/providers?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let providersUrl = URL(string: providersApiUrl + showId + endUrl)
-        
-        providerLabel.text =
-        
-        
-        
-    }
+//    func getProviders() {
+//
+//        let providersApiUrl = "https://api.themoviedb.org/3/tv/"
+//        let showId = tvShow["id"] as! String
+//        let endUrl = "/watch/providers?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+//        let providersUrl = URL(string: providersApiUrl + showId + endUrl)
+//
+//
+//        if(tvShowProviders["provider_name"] == "fuboTV") {
+//            var image: UIImage = UIImage(named: "fubotv")!
+//            watchProvidersCell.contentView.af.setImage(image)
+//        }
+//
+//    }
 
     /*
     // MARK: - Navigation
--- tvGuidance/TV-Guidance/SearchViewController.swift
+++ tvGuidance/TV-Guidance/SearchViewController.swift
@@ -18,7 +18,7 @@ class SearchViewController: UIViewController {
         self.view.addGestureRecognizer(tap)
 
         // Do any additional setup after loading the view.
-        let url = URL(string: "https://api.themoviedb.org/3/search/tv?api_key=081cd3e558e599982d21d7d81eecb1cc")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/76341?api_key=081cd3e558e599982d21d7d81eecb1cc")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -42,14 +42,21 @@ class SearchViewController: UIViewController {
     @objc func dismisskeyboard(){
         self.view.endEditing(true)
     }
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
+        let cell = sender as! UICollectionViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let tvShow = tvShows[indexPath.row]
+        
         // Pass the selected object to the new view controller.
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.tvShow = tvShow
+        
     }
-    */
+    
 
 }
