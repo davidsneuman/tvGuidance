5d7ec9275ada30709819fa91fb55985bf6f59e2d
David Neuman <davidsneuman@gmail.com>
2021-05-28 10:29:16 -0400

    added assets, fixed bug

--- tvGuidance/TV-Guidance/SearchViewController.swift
+++ tvGuidance/TV-Guidance/SearchViewController.swift
@@ -41,40 +41,44 @@ class SearchViewController: UIViewController {
         let searchText = searchTextField.text?.replacingOccurrences(of: " ", with: "%20")
         
         //multisearch API Call
-        let url = URL(string: "https://api.themoviedb.org/3/search/multi?api_key=081cd3e558e599982d21d7d81eecb1cc&language=en-US&query=\(searchText ?? "")&page=1&include_adult=false")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-             // This will run when the network request returns
-             if let error = error {
-                    print(error.localizedDescription)
-             } else if let data = data {
-                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+        if(searchText != "")
+        {
+            let url = URL(string: "https://api.themoviedb.org/3/search/multi?api_key=081cd3e558e599982d21d7d81eecb1cc&language=en-US&query=\(searchText ?? "")&page=1&include_adult=false")!
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { (data, response, error) in
+                 // This will run when the network request returns
+                 if let error = error {
+                        print(error.localizedDescription)
+                 } else if let data = data {
+                        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
 
 
-                // Multisearch API Call results
-                self.tvShows = dataDictionary["results"] as! [[String: Any]]
-                
-                // filter results to only tv shows with title synopsis and poster
-                self.tvShows = self.tvShows.filter({ (tvShow:[String : Any]) -> Bool in
+                    // Multisearch API Call results
+                    self.tvShows = dataDictionary["results"] as! [[String: Any]]
+                    
+                    // filter results to only tv shows with title synopsis and poster
+                    self.tvShows = self.tvShows.filter({ (tvShow:[String : Any]) -> Bool in
 
-                    let tvTitle = tvShow["original_name"] as? String ?? ""
-                    let movieTitle = tvShow["original_title"] as? String ?? ""
-                    let synopsis = tvShow["overview"] as? String ?? ""
-                    let posterPath = tvShow["poster_path"] as? String ?? ""
+                        let tvTitle = tvShow["original_name"] as? String ?? ""
+                        let movieTitle = tvShow["original_title"] as? String ?? ""
+                        let synopsis = tvShow["overview"] as? String ?? ""
+                        let posterPath = tvShow["poster_path"] as? String ?? ""
 
-                    return ((tvTitle != "" || movieTitle != "") && synopsis != "" && (posterPath != ""))
+                        return ((tvTitle != "" || movieTitle != "") && synopsis != "" && (posterPath != ""))
 
 
-                })
-                
-                //Segue on button press
-                self.performSegue(withIdentifier: "resultsSegue", sender: self)
+                    })
+                    
+                    //Segue on button press
+                    self.performSegue(withIdentifier: "resultsSegue", sender: self)
 
-             }
+                 }
+            }
+            
+            
+            task.resume()
         }
-        
-        task.resume()
     }
 
     
